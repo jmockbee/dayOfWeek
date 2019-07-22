9a560533c3ed851f6fadd59a3984e53465b27642
Jon Mockbee <mockbee1000@gmail.com>
2019-07-22 19:48:44 -0400

    initial commit

---
+++ src/com/JonMockbee/Main.java
@@ -0,0 +1,17 @@
+package com.JonMockbee;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static void printDayOfTheWeek( int day) {
+    switch (day){
+
+        case 0:
+            System.out.println("Sunday");
+    }
+
+    }
+}
